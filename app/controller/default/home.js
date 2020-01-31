a2274cfb05baa1fb9ec664e5b69aa259419bb8d4
frontendgod <3054750147@qq.com>
2020-01-31 23:31:58 +0800

    p14

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -3,13 +3,10 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    this.ctx.body = 'api接口11'
-  }
-  async list() {
-		const { ctx } = this;
-		ctx.body = 'hi, list';
-	}
+    async index() {
+        this.ctx.body = 'api接口...'
+    }
+  
 }
 
 module.exports = HomeController;
